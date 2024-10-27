e23a8c752ec5d50005bfec2ff333ee55b01d3eea
Gemi <githuba122@gmail.com>
2024-10-27 13:19:35 +0700

    belajar link & navigation

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,20 +1,55 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
-    <nav className="flex bg-gray-800 px-5">
-      <h1 className="text-white py-2">Navbar</h1>
-      <ul className="flex ml-5 items-center">
-        <Link href="/">
-          <li className="mr-3 text-blue-300 cursor-pointer">Home</li>
-        </Link>
-        <Link href="/about">
-          <li className="mr-3 text-blue-300 cursor-pointer">About</li>
-        </Link>
-        <Link href="/about/profile">
-          <li className="mr-3 text-blue-300 cursor-pointer">Profile</li>
-        </Link>
-      </ul>
+    <nav className="flex bg-gray-800 px-5 items-center justify-between">
+      <div className="flex items-center">
+        <h1 className="text-white py-2">Navbar</h1>
+        <ul className="flex ml-5">
+          <Link href="/">
+            <li
+              className={`mr-3 ${
+                pathname === "/" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li
+              className={`mr-3 ${
+                pathname === "/about" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              About
+            </li>
+          </Link>
+          <Link href="/about/profile">
+            <li
+              className={`mr-3 ${
+                pathname === "/about/profile" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Profile
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <button
+          onClick={() => router.push("/login")}
+          className="bg-white rounded-md px-3 py-1 text-sm"
+        >
+          Login
+        </button>
+      </div>
     </nav>
   );
 };
