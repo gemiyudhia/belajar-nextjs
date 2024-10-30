9e916d03b66e9d8b7d65725776a4bd10e565606e
Gemi <githuba122@gmail.com>
2024-10-30 14:50:57 +0700

    belajar data fetching

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -3,18 +3,24 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
   {
     id: 1,
-    name: "sepatu baru",
+    title: "sepatu baru",
     price: 100000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png",
   },
   {
     id: 2,
-    name: "sepatu lama",
+    title: "sepatu lama",
     price: 200000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/3157d472-63ce-457d-9723-b087da2666f5/AIR+JORDAN+1+RETRO+LOW+OG.png",
   },
   {
     id: 3,
-    name: "sepatu bekas",
+    title: "sepatu bekas",
     price: 300000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/3bdf52f5-1615-4bdf-9936-355332055c21/JORDAN+TATUM+2+PF.png",
   },
 ];
 
--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,16 +1,63 @@
-import React from "react";
-
-type DetailProductPageProps = {
+type ProductPageProps = {
   params: {
     slug: string[];
   };
 };
 
-const DetailProductPage = ({ params }: DetailProductPageProps) => {
-  console.log(params);
+const getData = async () => {
+  // const res = await fetch("https://fakestoreapi.com/products");
+  const res = await fetch("http://localhost:3000/api/product", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed fetch data");
+  }
+
+  return res.json();
+};
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+  const products = await getData();
+
+  console.log(products);
   return (
-    <div>
-      <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1>
+    <div className="grid grid-cols-4 place-items-center mt-5">
+      {/* <h1>{params.slug ? " Product Page" : "Product Page"}</h1> */}
+      {products.data.length > 0 &&
+        products.data.map((product: any) => (
+          <div
+            key={product.key}
+            className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5"
+          >
+            <a href="#">
+              <img
+                className="p-8 rounded-t-lg object-cover h-96 w-full"
+                src={product.image}
+                alt="product image"
+              />
+            </a>
+            <div className="px-5 pb-5">
+              <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                  {product.title}
+                </h5>
+              </a>
+
+              <div className="flex items-center justify-between mt-3">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                  $ {product.price}
+                </span>
+                <a
+                  href="#"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
       {params.slug && (
         <>
           <h2>Category : {params.slug[0]}</h2>
@@ -22,4 +69,4 @@ const DetailProductPage = ({ params }: DetailProductPageProps) => {
   );
 };
 
-export default DetailProductPage;
+export default ProductPage;
