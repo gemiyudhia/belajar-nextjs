0cd1765118cd5a070b4cff04f4f9193121cda0e9
Gemi <githuba122@gmail.com>
2024-11-02 13:31:12 +0700

    belajar auth register firebase

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,11 +1,43 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const { push } = useRouter();
+  const [isError, setIsError] = useState("");
+  const [isLoading, setIsloading] = useState(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsError("");
+    setIsloading(true);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    if (res.status === 200) {
+      e.target.reset();
+      setIsloading(false);
+      push("/login");
+    } else {
+      setIsError("Email already exist");
+      setIsloading(false);
+    }
+  };
+
   return (
-    <div className="h-screen w-full flex items-center justify-center">
+    <div className="h-screen w-full flex items-center justify-center flex-col">
+      {isError !== "" && (
+        <div className="text-red-600 font-bold mb-3">{isError}</div>
+      )}
       <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" action="#">
+        <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
           <h3 className="text-xl font-medium text-gray-900 dark:text-white">
             Sign up to our platform
           </h3>
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,9 @@
+import { register } from "@/lib/firebase/service";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+  // memasukkan data ke database
+  const res = await register(req);
+return NextResponse.json({status: res.status, message: res.message}, {status: res.statusCode});
+}
