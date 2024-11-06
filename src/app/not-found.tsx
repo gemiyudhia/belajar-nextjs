9aadbfe9fbd92b3b1c15faa5b1154187cd61a600
Gemi <githuba122@gmail.com>
2024-11-06 16:18:32 +0700

    beljar custom error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1>404: Page Not Found</h1>
+      <h2>Sorry mate</h2>
+      <Link href='/' className="bg-blue-500 p-3 rounded-md text-white font-medium">Back to Home</Link>
+    </div>
+  )
+}
